204176d911ac8aaa1e09f5853a0767dbac5923e6
kishin222 <kishin222frwak@gmail.com>
2020-08-04 21:54:20 +0900

    Oricon Newsにそっくり

--- src/App.js
+++ src/App.js
@@ -9,16 +9,11 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import { spacing } from '@material-ui/system';
 // import logo from "./img/cd-image.png"; // with import
 import Box from "@material-ui/core/Box";
 import axios from "axios";
 
-axios
-  .get("https://safe-headland-46948.herokuapp.com/api/v1/releaseInfo")
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  });
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,8 +31,8 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 400,
   },
   image: {
-    width: 128,
-    height: 128,
+    width: 64,
+    height: 64,
   },
   img: {
     margin: "auto",
@@ -48,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
-  const [releaseInfo, setreleaseInfo] = useState({
+  const [releaseInfo, setReleaseInfo] = useState({
     "2020/07/03": [
       {
         title: "人生ふたり坂",
@@ -78,6 +73,12 @@ export default function App() {
       },
     ],
   })
+  axios
+  .get("https://safe-headland-46948.herokuapp.com/api/v1/releaseInfo/single")
+  .then(function (response) {
+    // handle success
+    setReleaseInfo(response)
+  })
   const releaseInfoKeys = Object.keys(releaseInfo)
   console.log(releaseInfoKeys)
   const classes = useStyles();
@@ -94,8 +95,9 @@ export default function App() {
   // const aiueo2 = Object.keys(aiueo)
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar position="static" color="#FFF">
+        <Toolbar
+        >
           <IconButton
             edge="start"
             className={classes.menuButton}
@@ -104,8 +106,10 @@ export default function App() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
+          <Typography variant="h6" className={classes.title} >
+            <Box textAlign="center">
+              リリース情報
+            </Box>
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
@@ -114,27 +118,33 @@ export default function App() {
         <div>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Box bgcolor="#757de8" p={2}>
-                {item}
-              </Box>
+              <Typography variant="subtitle2">
+                <Box bgcolor="#f3f3f3" p={2} fontWeight="fontWeightBold" padding={1} >
+                  {item} 発売
+                </Box>
+              </Typography>
             </Grid>
           </Grid>
           {releaseInfo[item].map((item2) => (
             <Paper className={classes.paper}>
               <Grid container spacing={2}>
-                <Grid item>
+                <Grid item >
                   <ButtonBase className={classes.image}>
-                    <img src={item2.migSrc} alt="img"/>
+                    <img src={item2.migSrc} alt="img" height="100%" />
                   </ButtonBase>
                 </Grid>
-                <Grid item xs={6} sm container>
+                <Grid item xs={9} sm container>
                   <Grid item xs container direction="column" spacing={2}>
                     <Grid item xs>
-                      <Typography gutterBottom variant="subtitle1">
-                        {item2.title}
+                      <Typography variant="caption">
+                        <Box fontWeight="fontWeightBold" lineHeight={1.2} paddingBottom={0.5}>
+                          {item2.title}
+                        </Box>
                       </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        {item2.artist}
+                      <Typography variant="caption" gutterBottom >
+                        <Box color="#4F4F4F">
+                          {item2.artist}
+                        </Box>
                       </Typography>
                     </Grid>
                   </Grid>
@@ -144,9 +154,6 @@ export default function App() {
           ))}
         </div>
       ))}
-      <button onClick={() => setreleaseInfo()}>
-        Click me
-      </button>
     </div>
   );
 }
