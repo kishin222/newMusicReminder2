0529efb04f9faca2d5d948c411cc070be526b73f
kishin222 <kishin222frwak@gmail.com>
2020-10-21 00:16:23 +0900

    アルバム情報取得可能

--- src/pages/home.js
+++ src/pages/home.js
@@ -64,6 +64,7 @@ function a11yProps(index) {
 
 
 const Home = () => {
+  //シングル
   const [releaseInfo, setReleaseInfo] = useState({
   })
   useEffect(() => {
@@ -75,6 +76,18 @@ const Home = () => {
     getUser()
   }, [])
   const releaseInfoKeys = Object.keys(releaseInfo)
+  //アルバム
+  const [releaseInfoAlbum, setReleaseInfoAlbum] = useState({
+  })
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios.get('https://new-music-notification-01.an.r.appspot.com/api/v1/releaseInfo/album');
+      // handle success
+      setReleaseInfoAlbum(response.data)
+    }
+    getUser()
+  }, [])
+  const releaseInfoKeysAlbum = Object.keys(releaseInfoAlbum)
   const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
@@ -100,9 +113,8 @@ const Home = () => {
             variant="fullWidth"
             aria-label="full width tabs example"
           >
-            <Tab label="Item One" {...a11yProps(0)} />
-            <Tab label="Item Two" {...a11yProps(1)} />
-            <Tab label="Item Three" {...a11yProps(2)} />
+            <Tab label="シングル" {...a11yProps(0)} />
+            <Tab label="アルバム" {...a11yProps(1)} />
           </Tabs>
         </AppBar>
         <SwipeableViews
@@ -111,61 +123,101 @@ const Home = () => {
           onChangeIndex={handleChangeIndex}
         >
           <TabPanel value={value} index={0} dir={theme.direction}>
-            Item One
-        </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            Item Two
-        </TabPanel>
-          <TabPanel value={value} index={2} dir={theme.direction}>
-            Item Three
-        </TabPanel>
-        </SwipeableViews>
-      </div>
-      {releaseInfoKeys.map((item, index) => (
-        <div key={index}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Typography variant="subtitle2">
-                <Box bgcolor="#f3f3f3" p={2} fontWeight="fontWeightBold" padding={1} >
-                  {item} 発売
+            {releaseInfoKeys.map((item, index) => (
+              <div key={index}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography variant="subtitle2">
+                      <Box bgcolor="#f3f3f3" p={2} fontWeight="fontWeightBold" padding={1} >
+                        {item} 発売
                 </Box>
-              </Typography>
-            </Grid>
-          </Grid>
-          {releaseInfo[item].map((item2, index) => (
-            <Paper className={classes.paper} key={index}>
-              <Grid container
-                spacing={2}
-                onClick={() => {
-                  const searchQuery = "https://www.google.com/search?q=" + item2.title + " " + item2.artist
-                  window.open(searchQuery, '_blank')
-                }}>
-                <Grid item >
-                  <ButtonBase className={classes.image} >
-                    <img src={item2.imgSrc} alt="img" height="100%" />
-                  </ButtonBase>
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item xs={9} sm container alignItems="center" justify="center">
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                      <Typography variant="caption">
-                        <Box fontWeight="fontWeightBold" lineHeight={1.2} paddingBottom={0.5}>
-                          {item2.title}
-                        </Box>
-                      </Typography>
-                      <Typography variant="caption" gutterBottom >
-                        <Box color="#4F4F4F">
-                          {item2.artist}
-                        </Box>
+                {releaseInfo[item].map((item2, index) => (
+                  <Paper className={classes.paper} key={index}>
+                    <Grid container
+                      spacing={2}
+                      onClick={() => {
+                        const searchQuery = "https://www.google.com/search?q=" + item2.title + " " + item2.artist
+                        window.open(searchQuery, '_blank')
+                      }}>
+                      <Grid item >
+                        <ButtonBase className={classes.image} >
+                          <img src={item2.imgSrc} alt="img" height="100%" />
+                        </ButtonBase>
+                      </Grid>
+                      <Grid item xs={9} sm container alignItems="center" justify="center">
+                        <Grid item xs container direction="column" spacing={2}>
+                          <Grid item xs>
+                            <Typography variant="caption">
+                              <Box fontWeight="fontWeightBold" lineHeight={1.2} paddingBottom={0.5}>
+                                {item2.title}
+                              </Box>
+                            </Typography>
+                            <Typography variant="caption" gutterBottom >
+                              <Box color="#4F4F4F">
+                                {item2.artist}
+                              </Box>
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                ))}
+              </div>
+            ))}
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            {releaseInfoKeysAlbum.map((item, index) => (
+                <div key={index}>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                      <Typography variant="subtitle2">
+                        <Box bgcolor="#f3f3f3" p={2} fontWeight="fontWeightBold" padding={1} >
+                          {item} 発売
+                  </Box>
                       </Typography>
                     </Grid>
                   </Grid>
-                </Grid>
-              </Grid>
-            </Paper>
-          ))}
-        </div>
-      ))}
+                  {releaseInfoAlbum[item].map((item2, index) => (
+                    <Paper className={classes.paper} key={index}>
+                      <Grid container
+                        spacing={2}
+                        onClick={() => {
+                          const searchQuery = "https://www.google.com/search?q=" + item2.title + " " + item2.artist
+                          window.open(searchQuery, '_blank')
+                        }}>
+                        <Grid item >
+                          <ButtonBase className={classes.image} >
+                            <img src={item2.imgSrc} alt="img" height="100%" />
+                          </ButtonBase>
+                        </Grid>
+                        <Grid item xs={9} sm container alignItems="center" justify="center">
+                          <Grid item xs container direction="column" spacing={2}>
+                            <Grid item xs>
+                              <Typography variant="caption">
+                                <Box fontWeight="fontWeightBold" lineHeight={1.2} paddingBottom={0.5}>
+                                  {item2.title}
+                                </Box>
+                              </Typography>
+                              <Typography variant="caption" gutterBottom >
+                                <Box color="#4F4F4F">
+                                  {item2.artist}
+                                </Box>
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Paper>
+                  ))}
+                </div>
+              ))}
+          </TabPanel>
+        </SwipeableViews>
+      </div>
     </div>
   );
 }
