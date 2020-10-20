5c64b9f8ab32a51d7d53100f1085354c990aa758
kishin222 <kishin222frwak@gmail.com>
2020-10-20 23:19:37 +0900

    スワイプタブ追加

--- src/styles/useStyles.js
+++ src/styles/useStyles.js
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    width: 400,
   },
   title: {
     flexGrow: 1,
