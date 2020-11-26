733d211c1440f209467556268f08743256471353
kishin222 <kishin222frwak@gmail.com>
2020-11-26 23:06:55 +0900

    不要な行削除

--- src/pages/favorite.js
+++ src/pages/favorite.js
@@ -200,53 +200,6 @@ const Favorite = () => {
       {noNewFavoriteArtistReleaseMessageAlbum}
     </div>
   );
-  // return (
-  //   <div className={classes.root}>
-  //     <Header />
-  //     {favoriteSongReleaseDates.map((item, index) => (
-  //       <div key={index}>
-  //         <Grid container spacing={3}>
-  //           <Grid item xs={12}>
-  //             <Typography variant="subtitle2">
-  //               <Box bgcolor="#f3f3f3" p={2} fontWeight="fontWeightBold" padding={1} >
-  //                 {item} 発売
-  //               </Box>
-  //             </Typography>
-  //           </Grid>
-  //         </Grid>
-  //         {releaseInfoFavoriteValidDate[item].map((item2, index) => (
-  //           <Paper className={classes.paper} key={index}>
-  //             <Grid container spacing={2}>
-  //               <Grid item >
-  //                 <ButtonBase className={classes.image}>
-  //                   <img src={item2.imgSrc} alt="img" height="100%" />
-  //                 </ButtonBase>
-  //               </Grid>
-  //               <Grid item xs={9} sm container alignItems="center" justify="center">
-  //                 <Grid item xs container direction="column" spacing={2}>
-  //                   <Grid item xs>
-  //                     <Typography variant="caption">
-  //                       <Box fontWeight="fontWeightBold" lineHeight={1.2} paddingBottom={0.5}>
-  //                         {item2.title}
-  //                       </Box>
-  //                     </Typography>
-  //                     <Typography variant="caption" gutterBottom >
-  //                       <Box color="#4F4F4F">
-  //                         {item2.artist}
-  //                       </Box>
-  //                     </Typography>
-  //                   </Grid>
-  //                 </Grid>
-  //               </Grid>
-  //             </Grid>
-  //           </Paper>
-  //         ))}
-  //       </div>
-  //     ))}
-  //     {noArtistMessage}
-  //     {noNewFavoriteArtistReleaseMessage}
-  //   </div>
-  // );
 }
 
 export default Favorite;
