808e915a33da3de74eb09251e8dd8b6cdc17fb48
kishin222 <kishin222frwak@gmail.com>
2020-12-05 16:06:41 +0900

    format

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,11 +9,11 @@ const About = () => {
   const classes = useStyles();
   return (
     <>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/>ようこそ、<br/>LISNAへ<br/><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br />ようこそ、<br />LISNAへ<br /><br /></Typography>
       <Box align='center'>
-        <img src="/lisna_logo_v3.png" alt="img" width="150"/>
+        <img src="/lisna_logo_v3.png" alt="img" width="150" />
       </Box>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
       <Box align='center'>
         <Button
           className={classes.greenButton}
@@ -28,57 +28,57 @@ const About = () => {
           </Link>
         </Button>
       </Box>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
       <Box align='center'>
-        <img src="/screenShot.png" alt="img" width="300"/>
+        <img src="/screenShot.png" alt="img" width="300" />
       </Box>
-      <Typography variant='body2' align='center'className={classes.textGray}>
-        LISNA（リスナ）は、<br/>
-        Official髭男dism、King Gnu、菅田将暉、<br/>
-        あいみょんなど400組以上のアーティスト、<br/>
-        600枚以上の最新リリースCDが集まる<br/>
+      <Typography variant='body2' align='center' className={classes.textGray}>
+        LISNA（リスナ）は、<br />
+        Official髭男dism、King Gnu、菅田将暉、<br />
+        あいみょんなど400組以上のアーティスト、<br />
+        600枚以上の最新リリースCDが集まる<br />
         新曲まとめサイトです。
       </Typography>
-      <Typography variant='h4' align='center'className={classes.textGray}>
-        <br/>LISNA<br/>便利な3つの機能<br/><br/>
+      <Typography variant='h4' align='center' className={classes.textGray}>
+        <br />LISNA<br />便利な3つの機能<br /><br />
       </Typography>
       <Box align='center'>
-        <img src="/favoriteIcon.png" alt="img" width="100"/>
+        <img src="/favoriteIcon.png" alt="img" width="100" />
       </Box>
-      <Typography variant='h5' align='center'className={classes.textGray}>
-        好みのアーティストを<br/>お気に入り
+      <Typography variant='h5' align='center' className={classes.textGray}>
+        好みのアーティストを<br />お気に入り
       </Typography>
-      <Typography variant='body2' align='center'className={classes.textGray}>
-        まずは自分の好きなアーティストを<br/>
-        お気に入り登録<br/>
-        お気に入りアーティストに絞り込んで<br/>
+      <Typography variant='body2' align='center' className={classes.textGray}>
+        まずは自分の好きなアーティストを<br />
+        お気に入り登録<br />
+        お気に入りアーティストに絞り込んで<br />
         新曲リストを確認できます
       </Typography>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
       <Box align='center'>
-        <img src="/searchIcon.png" alt="img" width="100"/>
+        <img src="/searchIcon.png" alt="img" width="100" />
       </Box>
-      <Typography variant='h5' align='center'className={classes.textGray}>
+      <Typography variant='h5' align='center' className={classes.textGray}>
         気になった曲はすぐ検索
       </Typography>
-      <Typography variant='body2' align='center'className={classes.textGray}>
-        新曲の一覧からすぐ検索<br/>
+      <Typography variant='body2' align='center' className={classes.textGray}>
+        新曲の一覧からすぐ検索<br />
         お好きなサービスで新曲を確認できます
       </Typography>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
       <Box align='center'>
-        <img src="/pushIcon.png" alt="img" width="100"/>
+        <img src="/pushIcon.png" alt="img" width="100" />
       </Box>
-      <Typography variant='h5' align='center'className={classes.textGray}>
-        新曲通知が届く<br/>
+      <Typography variant='h5' align='center' className={classes.textGray}>
+        新曲通知が届く<br />
         （準備中）
       </Typography>
-      <Typography variant='body2' align='center'className={classes.textGray}>
-        一度お気に入りアーティストを設定したら<br/>
-        次から自動的にお知らせ<br/>
+      <Typography variant='body2' align='center' className={classes.textGray}>
+        一度お気に入りアーティストを設定したら<br />
+        次から自動的にお知らせ<br />
         注目の新譜を見逃すことはありません
       </Typography>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
       <Box align='center'>
         <Button
           className={classes.greenButton}
@@ -93,7 +93,7 @@ const About = () => {
           </Link>
         </Button>
       </Box>
-      <Typography variant='h4' align='center' className={classes.textGray}><br/></Typography>
+      <Typography variant='h4' align='center' className={classes.textGray}><br /></Typography>
 
     </>
   );
